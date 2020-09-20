21e6848fed38d55aa91e4dc2724e0195801a5b7d
unknown <phanudetkawilai2016@gmail.com>
2020-09-20 20:50:45 +0700

    add

--- src/network/utils/get-data.utils.ts
+++ src/network/utils/get-data.utils.ts
@@ -1,4 +1,5 @@
 import { Observable } from 'rxjs'
+import { OID_LIST } from 'src/const/app.const'
 
 export async function getOs(device: any): Promise<unknown> {
   const result = new Observable(observer => {
@@ -66,9 +67,49 @@ export async function getTemperature(device: any): Promise<unknown> {
 export async function getInbound(device: any): Promise<unknown> {
   const result = new Observable(observer => {
     const inbound = []
+
     device.getSubtree({ oid: [1, 3, 6, 1, 2, 1, 2, 2, 1, 10] }, (err, varbinds) => {
       for (const varbind of varbinds) {
-        inbound.push(Math.round(varbind.value / 1048576))
+
+        OID_LIST.forEach(e => {
+          if (e.ip == "10.99.1.1") {
+            let is_same = varbind.oid.every(function(element, index) {
+              return element === e.oid[index]
+            })
+            if (is_same == false) {
+              //console.log("=========== push ===========", e.ip)
+              //inbound.push(Math.round(varbind.value / 1048576))
+            } else {
+              //console.log("=========== no push ===========", varbind.oid)
+            }
+          }
+
+          if (e.ip == "10.99.4.1") {
+            let is_same = varbind.oid.every(function(element, index) {
+              return element === e.oid[index]
+            })
+            if (is_same == false) {
+              //console.log("=========== push ===========", e.ip)
+              //inbound.push(Math.round(varbind.value / 1048576))
+            } else {
+              //console.log("=========== no push ===========", varbind.oid)
+            }
+          }
+
+          if (e.ip == "10.99.6.1") {
+            let is_same = varbind.oid.every(function(element, index) {
+              return element === e.oid[index]
+            })
+            if (is_same == false) {
+              //console.log("=========== push ===========", e.ip)
+              //inbound.push(Math.round(varbind.value / 1048576))
+            } else {
+              console.log("=========== no push ===========", e.ip, varbind.oid)
+            }
+          }
+        })
+
+        // inbound.push(Math.round(varbind.value / 1048576))
       }
       observer.next(inbound)
       observer.complete()
